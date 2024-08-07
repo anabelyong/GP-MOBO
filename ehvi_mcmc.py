0d955c851945c93d0618d084dc722d903211fe81
bananabelyong <65429211+anabelyong@users.noreply.github.com>
2024-08-07 10:02:39 +0100

    MC Integration method checked

--- ehvi_mcmc.py
+++ ehvi_mcmc.py
@@ -1,5 +1,4 @@
-import cProfile
-import pstats
+#correct MC integration method implementation
 import numpy as np
 import matplotlib.pyplot as plt
 from dockstring.dataset import load_dataset
@@ -20,6 +19,7 @@ def expected_hypervolume_improvement(pred_means, pred_vars, reference_point, par
         var = pred_vars[i]
         cov = np.diag(var)
 
+        # Monte Carlo integration
         samples = np.random.multivariate_normal(mean, cov, size=N)
 
         hvi = 0.0
@@ -49,61 +49,6 @@ def ehvi_acquisition(query_smiles, known_smiles, known_Y, gp_means, gp_amplitude
 
     return ehvi_values
 
-def sample_from_gp_posterior(query_smiles, known_smiles, known_Y, gp_means, gp_amplitudes, gp_noises, N=100):
-    pred_means, pred_vars = independent_tanimoto_gp_predict(
-        query_smiles=query_smiles,
-        known_smiles=known_smiles,
-        known_Y=known_Y,
-        gp_means=gp_means,
-        gp_amplitudes=gp_amplitudes,
-        gp_noises=gp_noises,
-    )
-
-    samples = np.zeros((N, len(query_smiles), pred_means.shape[-1]))
-    for i in range(len(query_smiles)):
-        mean = pred_means[i]
-        var = pred_vars[i]
-        cov = np.diag(var)
-        samples[:, i, :] = np.random.multivariate_normal(mean, cov, size=N)
-    return samples
-
-def momcmc_sampling(known_smiles, known_Y, gp_means, gp_amplitudes, gp_noises, num_samples, temperature, desired_acceptance, reference_point, N=100):
-    """
-    1) Based on Pseudocode from MOMCMC: An efficient Monte Carlo method for multi-objective sampling over real parameter space
-    https://www.sciencedirect.com/science/article/pii/S089812211200569X
-    """
-    P = known_Y.copy()
-    N_points = P.shape[0]
-    acceptance_rate = 0.0
-
-    hv = Hypervolume(reference_point)
-
-    while acceptance_rate < desired_acceptance:
-        P_new = np.empty_like(P)
-        samples = sample_from_gp_posterior(known_smiles, known_smiles, known_Y, gp_means, gp_amplitudes, gp_noises, N)
-
-        for i in range(N_points):
-            # Select a random sample
-            sample_idx = np.random.randint(N)
-            v = samples[sample_idx, i, :]
-
-            if pareto_front(np.vstack([P, v]))[-1]:
-                fit_new = hv.compute(np.vstack([P, v]))
-                fit_old = hv.compute(P)
-                if np.random.rand() < np.exp((fit_old - fit_new) / temperature):
-                    P_new[i] = v
-                else:
-                    P_new[i] = P[i]
-            else:
-                P_new[i] = P[i]
-        acceptance_rate = np.mean(np.all(P_new == P, axis=1))
-        P = P_new
-        if acceptance_rate < desired_acceptance:
-            temperature *= 1.1
-        else:
-            temperature *= 0.9
-    return P
-
 def bayesian_optimization_loop(
     known_smiles,
     query_smiles,
@@ -118,8 +63,7 @@ def bayesian_optimization_loop(
 ):
     S_chosen = set()
     hypervolumes_bo = []
-    temperature = 1.0
-    desired_acceptance = 0.1
+    acquisition_values = []
 
     for iteration in range(n_iterations):
         print(f"Start BO iteration {iteration}. Dataset size={known_Y.shape}")
@@ -148,6 +92,7 @@ def bayesian_optimization_loop(
                 max_acq = ehvi_value
                 best_smiles = smiles
 
+        acquisition_values.append(max_acq)
         print(f"Max acquisition value: {max_acq}")
         if best_smiles:
             S_chosen.add(best_smiles)
@@ -165,26 +110,32 @@ def bayesian_optimization_loop(
         hypervolumes_bo.append(current_hypervolume)
         print(f"Hypervolume: {current_hypervolume}")
 
-        known_Y = momcmc_sampling(
-            known_smiles=known_smiles,
-            known_Y=known_Y,
-            gp_means=gp_means,
-            gp_amplitudes=gp_amplitudes,
-            gp_noises=gp_noises,
-            num_samples=known_Y.shape[0],
-            temperature=temperature,
-            desired_acceptance=desired_acceptance,
-            reference_point=reference_point,
-            N=100
-        )
+    return known_smiles, known_Y, hypervolumes_bo, acquisition_values
 
-    return known_smiles, known_Y, hypervolumes_bo
+def plot_results(hypervolumes, acquisition_values):
+    iterations = np.arange(1, len(hypervolumes) + 1)
+
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Iterations')
+    ax1.set_ylabel('Hypervolume', color='tab:blue')
+    ax1.plot(iterations, hypervolumes, 'o-', color='tab:blue', label='Hypervolume')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Acquisition Function Value', color='tab:red')
+    ax2.plot(iterations, acquisition_values, 'o-', color='tab:red', label='Acquisition Function Value')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+
+    fig.tight_layout()
+    plt.title('Hypervolume and Acquisition Function Value over BO Iterations')
+    plt.show()
 
 def plot_3d_pareto_and_samples(known_Y, pareto_Y):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
 
-    ax.scatter(known_Y[:, 0], known_Y[:, 1], known_Y[:, 2], c="blue", label="MOMCMC")
+    ax.scatter(known_Y[:, 0], known_Y[:, 1], known_Y[:, 2], c="blue", label="Samples")
     ax.scatter(pareto_Y[:, 0], pareto_Y[:, 1], pareto_Y[:, 2], c="red", label="Pareto Optimal Front")
 
     ax.set_xlabel("f1")
@@ -194,9 +145,6 @@ def plot_3d_pareto_and_samples(known_Y, pareto_Y):
     plt.show()
 
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
-
     DOCKSTRING_DATASET = load_dataset()
     ALL_SMILES = list(DOCKSTRING_DATASET["PPARD"].keys())[:10_000]
     initial_smiles = ALL_SMILES[:20]
@@ -209,7 +157,7 @@ if __name__ == "__main__":
     gp_amplitudes = np.asarray([1.0, 0.5, 1.0])
     gp_noises = np.asarray([1.0, 1e-4, 1e-1])
 
-    known_smiles_bo, known_Y_bo, hypervolumes_bo = bayesian_optimization_loop(
+    known_smiles_bo, known_Y_bo, hypervolumes_bo, acquisition_values_bo = bayesian_optimization_loop(
         known_smiles=known_smiles.copy(),
         query_smiles=query_smiles,
         known_Y=known_Y.copy(),
@@ -218,14 +166,9 @@ if __name__ == "__main__":
         gp_noises=gp_noises,
         n_iterations=20,
     )
-    """
-    TODO: clean up code to avoid having irrelevant stats and profiler results
-    """
-    profiler.disable()
-    stats = pstats.Stats(profiler).sort_stats("cumtime")
-    stats.print_stats()
 
     pareto_mask = pareto_front(known_Y_bo)
     pareto_Y_bo = known_Y_bo[pareto_mask]
 
     plot_3d_pareto_and_samples(known_Y_bo, pareto_Y_bo)
+    plot_results(hypervolumes_bo, acquisition_values_bo)
